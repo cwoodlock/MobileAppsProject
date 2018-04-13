e1d3d2838e2bc4c4c0856edb0ef47260c31cda37
COLM WOODLOCK - STUDENT <G00341460@gmit.ie>
2018-04-13 14:49:05 +0100

    added some functions that I might need and started on game area border function

--- Project/Project/Level1.xaml.cs
+++ Project/Project/Level1.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,11 +26,66 @@ namespace Project
         public Level1()
         {
             this.InitializeComponent();
+
+            addBorders();
+
+            //randomNum();
+            //Ellipse el1 = createBlueCircles();
+            //Ellipse el2 = createRedCircles();
         }
+
+        private void addBorders()
+        {
+            int iR;
+            int iC;
+
+            for(iR =0;iR < 8; iR++)
+            {
+                for (iC = 0; iC < 8; iC++)
+                {
+                    Border b = new Border();
+                    Border outOfBounds = new Border();
+
+                    //To get the position of the pieces on the board we must name the positions
+                    b.Name = iR.ToString() + "_" + iC.ToString();
+                }
+            }
+            
+        }//End addBorders
+
+        private static int randomNum()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 10);
+            return num;
+        }//End randomNum
+
+        private static Ellipse createRedCircles()
+        {
+ 
+            Ellipse redEllipse = new Ellipse();
+            redEllipse.Height = 50;
+            redEllipse.Width = 50;
+            redEllipse.StrokeThickness = 2;
+            redEllipse.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
+            redEllipse.Fill = new SolidColorBrush(Windows.UI.Colors.Red);
+            return redEllipse;
+        }//End addRedCircles
+
+        private static Ellipse createBlueCircles()
+        {
+            Ellipse blueEllipse = new Ellipse();
+            blueEllipse.Height = 50;
+            blueEllipse.Width = 50;
+            blueEllipse.StrokeThickness = 2;
+            blueEllipse.Stroke = new SolidColorBrush(Windows.UI.Colors.Black);
+            blueEllipse.Fill = new SolidColorBrush(Windows.UI.Colors.Blue);
+            return blueEllipse;
+        }//End addBlueCircles
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.GoBack();
-        }
+        }//End Button_Click
     }
 }
