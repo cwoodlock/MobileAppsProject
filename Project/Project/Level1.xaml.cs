071484e8aa604be97980befda5132f938cb8ccd4
COLM WOODLOCK - STUDENT <G00341460@gmit.ie>
2018-04-13 15:28:11 +0100

    Couldnt get my code working so adapted code from labs instead

--- Project/Project/Level1.xaml.cs
+++ Project/Project/Level1.xaml.cs
@@ -35,41 +35,90 @@ namespace Project
             //Ellipse el2 = createRedCircles();
         }
 
-        private void addBorders()
+        private void addBorders() //Adapted from code given to us in labs
         {
-            int iR;
-            int iC;
-
-            for(iR =0;iR < 8; iR++)
+            int row = 5;
+            int size = 75;
+            
+            try
             {
-                for (iC = 0; iC < 8; iC++)
-                {
-                    Border b = new Border();
-
-                    //To get the position of the pieces on the board we must name the positions
-                    b.Name = iR.ToString() + "_" + iC.ToString();
-
-                    //Set the colour of the border
-                    b.Background = new SolidColorBrush(Colors.Honeydew);
-
-                    //Set the page up for now like a chessboard so as to get my head around the logic
-                    if((iR + iC) % 2 == 0)
-                    {
-                        b.Background = new SolidColorBrush(Colors.HotPink);
-                    }
-
-                    b.SetValue(Grid.RowProperty, iR);
-                    b.SetValue(Grid.ColumnProperty, iC);
-
-                    b.HorizontalAlignment = HorizontalAlignment.Center;
-                    b.VerticalAlignment = VerticalAlignment.Center;
-
-                    b.Height = 10;
-                    b.Width = 10;
-
-                    gridBoard.Children.Add(b);
-                }
+               gridBoard.Children.Remove(FindName("ChessBoard") as Grid);
             }
+            catch
+            {
+            }
+            // create a grid object
+            Grid grdBoard = new Grid();
+
+            // give it a name, size, horizontal alignment, vertical align
+            // give it background colour, margin of 5, Grid.row = 1
+            grdBoard.Name = "ChessBoard";
+
+            //Set vertical and horizontal allignment
+            grdBoard.HorizontalAlignment = HorizontalAlignment.Center;
+            grdBoard.VerticalAlignment = VerticalAlignment.Center;
+
+            //Set the size of the squares
+            grdBoard.Height = size * row;
+            grdBoard.Width = size * row;
+            
+            //Set the background colour of the grid
+            grdBoard.Background = new SolidColorBrush(Colors.Gray);
+
+            grdBoard.SetValue(Grid.ColumnProperty, 1);
+            grdBoard.SetValue(Grid.RowProperty, 1);
+
+            // add row number of row definitions and column definitions
+            for (int i = 0; i < row; i++)
+            {
+                grdBoard.RowDefinitions.Add(new RowDefinition());
+                grdBoard.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            // add the gameboard to the root grid children collection
+            gridBoard.Children.Add(grdBoard);
+
+            // add a border object to each cell on the grid
+            // to add one border
+            // create the border object
+            Border brdr; // don't put this in the loop
+
+            int iR, iC;
+
+            for (iR = 0; iR < row; iR++) // on each row
+            {
+                for (iC = 0; iC < row; iC++)  // for each col on that row
+                {
+                  
+                    brdr = new Border();
+
+                    // give it height, width, horizontal & vertical align in centre
+                    brdr.Height = size * 0.98;
+                    brdr.Width = size * 0.98;
+
+                    // sq_R_C, no duplicates here 
+                    brdr.Name = iR.ToString() + iC.ToString();
+
+                    //Set the verical and horizontal alignment
+                    brdr.HorizontalAlignment = HorizontalAlignment.Center;
+                    brdr.VerticalAlignment = VerticalAlignment.Center;
+
+                    // set the Grid.col, grid.row property
+                    brdr.SetValue(Grid.RowProperty, iR);
+                    brdr.SetValue(Grid.ColumnProperty, iC);
+
+                    // give it a background colour
+                    brdr.Background = new SolidColorBrush(Colors.Red);
+                    if (0 == (iR + iC) % 2)
+                    {
+                        brdr.Background = new SolidColorBrush(Colors.Transparent);
+                    }
+                    // add it to the board children collection
+                    grdBoard.Children.Add(brdr);
+           
+                } // end iC
+            } // end iR
+ 
             
         }//End addBorders
 
